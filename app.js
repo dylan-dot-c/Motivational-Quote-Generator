b249afc687da17ea8a410bb00055fe12f30a0617
dylan-dot-c <78489976+dylan-dot-c@users.noreply.github.com>
2022-08-21 10:44:56 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const quoteArea = document.querySelector('.quote')
 const writer = document.querySelector('.author')
 const next = document.querySelector('#next-quote')
 const url = 'https://quote-garden.herokuapp.com/api/v3/quotes?genre=motivation&limit=3&'
+const clipboard = document.querySelector('#copy')
 
 // console.log(quoteArea, writer, next)
 
 async function getapi(url)
 {
-  var rand = Math.ceil(Math.random()*10)
+  var rand = Math.ceil(Math.random()*30)
+  // navigator.clipboard.writeText("HELLO WORLD")
   const response = await fetch(url+'page='+rand);
   var data = await response.json();
   const quotesArray = data.data
@@ -18,8 +20,19 @@ async function getapi(url)
   quoteArea.innerText = randQuote.quoteText
 
 }
+
+//event listeners
 next.addEventListener('click', function(){
   getapi(url);
 });
+
+clipboard.addEventListener('click', function(){
+  let text = quoteArea.innerText + '\n ~' + writer.innerText;
+  // alert(text)
+    navigator.clipboard.writeText(text);
+    alert('Copied!')
+
+
+})
 
 getapi(url);
